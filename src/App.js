7f2a262ce22ddedace80e757cae7569c88b462bf
Sergey Kudashev <sergey.m.kudashev@gmail.com>
2024-01-11 16:17:26 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Привет</h2>
+        <h3>Это первая страница в React</h3>
       </header>
     </div>
   );
---
+++ src/Page.js
@@ -0,0 +1,17 @@
+function Page() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Добро пожаловать!</h2>
+        <ul>
+            {/* Список */}
+            <li>Это первая строка</li>
+            <li>Это вторая строка</li>
+            <li>Это третья строка</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default Page;
